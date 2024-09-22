e3a130ce0291e5f69324c8cae5c5138cce88d9e1
tx0 <yeswatsai@gmail.com>
2024-09-22 19:43:09 +0700

    Update autoupdatebedrock.sh

--- autoupdatebedrock.sh
+++ autoupdatebedrock.sh
@@ -14,16 +14,14 @@ RANDVERSION=$(echo $((1 + $RANDOM % 4000)))
 if [ -e $DOWNLOAD_FILE ]
 then
     echo "Version is latest."
-    ./bedrock_server
-    exit 0
+    kill "$$"
 else
     echo "Version is not latest."
-    curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.$RANDVERSION.212 Safari/537.36" -H "Accept-Language: en" -o $DOWNLOAD_FILE $DOWNLOAD_URL
+    curl -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.$RANDVERSION.212 Safari/537.36" -H "Accept-Language: en" -o $DOWNLOAD_FILE https://minevps.net/cdn/$DOWNLOAD_FILE
 
     unzip -o $DOWNLOAD_FILE
     rm $DOWNLOAD_FILE
     find . -type f -name '*bedrock-server-*' -delete
     echo "" > $DOWNLOAD_FILE
-    ./bedrock_server
-    exit 0
+    kill "$$"
 fi
